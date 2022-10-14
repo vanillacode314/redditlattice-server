3edeea7ea54fbf41f2c4b4c2e9d7e705cb8e3ca1
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2022-10-14 19:39:33 +0530

    update user agent

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.route({
         const isGif = new URL(url).pathname.endsWith(".gif");
         const { statusCode, body } = await client(url, {
             headers: {
-                "User-Agent": "User-Agent:Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
+                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
             },
             maxRedirections: 3,
         });
--- src/index.ts
+++ src/index.ts
@@ -60,7 +60,7 @@ app.route({
     const { statusCode, body } = await client(url, {
       headers: {
         "User-Agent":
-          "User-Agent:Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
+          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
       },
       maxRedirections: 3,
     });
